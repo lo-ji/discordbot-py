2b0b3a28b66c23e158b63cee5e6c6efab44392b6
AmongRed <loji40018@gmail.com>
2023-04-01 04:18:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,7 @@ import discord
 from dotenv import load_dotenv
 import os
 import random
+from datetime import datetime
 load_dotenv()
 
 PREFIX = os.environ['PREFIX']
@@ -37,6 +38,12 @@ async def on_message(message):
 
     if message.content == '하오님':
         await message.channel.send(as_Hao[random.randrange(0, 1)])
+    
+    if message.content == '죽어라':
+        await message.channel.send(as_Hao[random.randrange(0, 2)])
+
+    if message.content == '내이름':
+        await message.channel.send(message.author.nick + '맞죠?')
 
 #---------------------------------------------------------------------------------------------------
 
@@ -52,9 +59,6 @@ async def on_message(message):
     if message.content.startswith('ㅠㅠㅠㅠㅠ'): 
         await message.channel.send(as_TT[random.randrange(0, 4)])
 
-    if message.content.startswith('ㅠㅠㅠㅠㅠ'):
-        await message.channel.send('뭐요')
-
     if message.content.startswith('만장님'):
         await message.channel.send('파란만장~~~ 네 인생도 파란만장, 내 인생도 파란만장. 아~ 인생 고달프다아')
 
@@ -64,6 +68,7 @@ async def on_message(message):
 
 #---------------------------------------------------------------------------------------------------
 as_LeeDonim = ['네에', '네?', '뭐요', '?', '말걸지마세요']
+as_Die = ['뒤질래', '님선', 'ㅗ']
 as_Hao = ['뭐!? 하오님이라고!??', '하오님은 언제나 이뻐! 멋져! 귀여워!']
 as_Hi = ['안녕하쇼', '안녕하십니끄아아아아아아아아아아아아아아아악', '안녕하냐', '밥은 먹고 다니냐', '카레라이스']
 as_kkk = ['ㅋㅋㅋ', '허파에 바람들었어요?', '암흑이론에 대한 설명', 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ', 'ㅋㅋ']
@@ -72,8 +77,31 @@ as_why = ['본인의 아이큐를 1부터 10중에 표현하자면',
           '돼지고기 김치찌개 맛내는 비법 진한 국물이 맛있는 돼지고기 김치찌개입니다.'
             +'물 2컵(250ml 기준), 돼지고기 찌개용(or 목살 250g), 신김치 200g, 김칫국물 5큰술',
             '왜', '궁금하다why', '예비군 언제 감?']
-#---------------------------------------------------------------------------------------------------
 
+self_ = ['뀨><',
+         '이야기는 끝났다. 박수쳐라',
+         ' 다른 사람에게 허용된다고 해서, 너에게도 허용되는건 아니다.​',
+         '사랑하는 사람들은 미친 사람이다',
+         '4월 생존신고 진행중입니다~! 디스코드 생존신고 채널에 생존신고 눌러주세요!' +
+         '닉네임도 생존신고 기간동안은 단톡방과 되도록 맞춰주세요!']
+#---------------------------------------------------------------------------------------------------
+# 시간
+@client.event
+async def on_time(message):
+    now = datetime.now()
+
+    if now.hour == 4:
+        await message.channel.send('점심시간~ 밥은 먹고 그려야죠')
+        return
+    
+    elif now.hour == 15 and now.minute == 13:
+        await message.channel.send('점심시간~ 밥은 먹고 그려야죠')
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------
 
 try:
     client.run(TOKEN)
