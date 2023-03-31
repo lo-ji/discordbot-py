0745cada42be762e7ecb00382e799c200e738304
AmongRed <loji40018@gmail.com>
2023-04-01 01:22:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,7 @@ async def on_message(message):
 
     if message.content.startswith('hello'):
         await message.channel.send('Hello!')
+              message.channel.send('H222222')
         
     if message.content.startswith(f'{PREFIX}RandomNumber'):
         await message.channel.send(str(random.randrange(1,100))) 
