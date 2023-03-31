f4c850c173332fd5cee861795c7b0d97ef6faf00
AmongRed <loji40018@gmail.com>
2023-04-01 01:34:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-PREFIX = os.environ['PREFIX']
+@PREFIX = os.environ['PREFIX']
+PREFIX = '!'
 TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
