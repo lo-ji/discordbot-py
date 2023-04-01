9acfdc5fc74fd0956cc86ed12ad26fe4883d5a9d
AmongRed <loji40018@gmail.com>
2023-04-02 01:45:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ load_dotenv()
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
